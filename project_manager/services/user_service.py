8fe1bb6e2f76dffe64fbabb1ec56e3dd4070ad6b
LuisGrigore <luiscristiangrigore11@gmail.com>
2025-01-27 16:51:44 +0100

    find user by id in user service fixed

--- project_manager/services/user_service.py
+++ project_manager/services/user_service.py
@@ -1,12 +1,12 @@
 from typing import Callable, Optional
-from returns.result import Result, Success, Failure
+from returns.result import Result
 
 from project_manager.dtos import UserCreateDto, UserGetDto
 from project_manager.errors.my_errors import UserNotPersistedError, UserNotFoundError
 from project_manager.mappers import user_mapper
 from project_manager.model import UserModel
 from project_manager.repos import user_repos
-from project_manager.services.base_functs import save_entity
+from project_manager.services.base_functs import save_entity, find_by_id
 
 
 def create_user(user_dto:UserCreateDto) -> Result[UserGetDto, UserNotPersistedError]:
@@ -17,8 +17,5 @@ def create_user(user_dto:UserCreateDto) -> Result[UserGetDto, UserNotPersistedEr
 
 def find_user_by_id(uid: int, find_in_db_funct: Callable[[int],Optional[UserModel]]) -> Result[UserGetDto, UserNotFoundError]:
     '''hola'''
-    user = find_in_db_funct(uid)
-    if user:
-        return Success(UserGetDto(user.name))
-    return Failure(UserNotFoundError())
+    return find_by_id(uid,find_in_db_funct,UserNotFoundError(),user_mapper.user_model_to_user_get_dto)
 
