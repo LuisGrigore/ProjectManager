9d05819631e54198a5872ce854fdf7e7249fc4c9
LuisGrigore <luiscristiangrigore11@gmail.com>
2025-01-27 16:51:20 +0100

    find by id base funct

--- project_manager/services/base_functs.py
+++ project_manager/services/base_functs.py
@@ -8,3 +8,10 @@ def save_entity(create_dto, error, persist_funct, create_dto_to_model_funct, mod
         get_dto = model_to_get_dto_funct(model_persisted)
         return Success(get_dto)
     return Failure(error)
+
+def find_by_id(id,find_in_db_funct,error, model_to_get_dto_funct):
+    '''hola'''
+    model = find_in_db_funct(id)
+    if model:
+        return Success(model_to_get_dto_funct(model))
+    return Failure(error)
